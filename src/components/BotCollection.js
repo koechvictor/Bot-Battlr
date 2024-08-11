4a131c9c5bf1b8d6055fe6f5b78bb8021607c636
koech victor <kipkiruiv408@gmail.com>
2024-08-11 14:35:46 +0000

    update

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -3,13 +3,17 @@ import BotCard from "./BotCard";
 
 function BotCollection({ botCollection, action, removeCard }) {
     return (
-        <div className="ui four column grid">
-            <div className="row">
-                {botCollection && botCollection.map((bot) => {
-                    return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />
-                })}
+        <>
+            <h1 class="title-header">Bot Collection</h1>
+            <div className="ui four column grid">
+                <div className="row">
+                    {botCollection && botCollection.map((bot) => {
+                        return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />
+                    })}
+                </div>
             </div>
-        </div>
+
+        </>
     );
 }
 
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -3,15 +3,18 @@ import BotCard from "./BotCard";
 function YourBotArmy({ bots, action, removeCard }) {
 
     return (
-        <div className="ui segment inverted olive bot-army">
-            <div className="ui five column grid">
-                <div className="row bot-army-row">
-                    {bots && bots.map((bot) => {
-                        return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />
-                    })}
+        <>
+            <div className="ui segment inverted olive bot-army">
+                <h1>Enlisted Army</h1>
+                <div className="ui five column grid">
+                    <div className="row bot-army-row">
+                        {bots && bots.map((bot) => {
+                            return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
