ec14c76a130ab6488e2416b9839ed8c9b7a7efea
koech victor <kipkiruiv408@gmail.com>
2024-08-11 13:40:25 +0000

    update

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,18 +1,18 @@
 import React from "react";
+import BotCard from "./BotCard";
+function YourBotArmy({ bots, action, removeCard }) {
 
-function YourBotArmy() {
-  //your bot army code here...
-
-  return (
-    <div className="ui segment inverted olive bot-army">
-      <div className="ui five column grid">
-        <div className="row bot-army-row">
-          {/*...and here...*/}
-          Your Bot Army
+    return (
+        <div className="ui segment inverted olive bot-army">
+            <div className="ui five column grid">
+                <div className="row bot-army-row">
+                    {bots && bots.map((bot) => {
+                        return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />
+                    })}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default YourBotArmy;
