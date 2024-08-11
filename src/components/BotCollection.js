01e28b51d607181a12fd0fa52006e61313bfde46
koech victor <kipkiruiv408@gmail.com>
2024-08-11 12:06:14 +0000

    bot collection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,12 @@
 import React from "react";
 import BotCard from "./BotCard";
 
-function BotCollection() {
+function BotCollection({ botCollection, action, removeCard }) {
     return (
         <div className="ui four column grid">
             <div className="row">
                 {botCollection && botCollection.map((bot) => {
-                    return <BotCard />
+                    return <BotCard key={bot.id} bot={bot} action={action} removeCard={removeCard} />
                 })}
             </div>
         </div>
