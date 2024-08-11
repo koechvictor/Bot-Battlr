32291ecd1fec76d6827331f7700777f16e23f7d9
koech victor <kipkiruiv408@gmail.com>
2024-08-11 13:37:51 +0000

    add filter and delete functions

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,17 +1,82 @@
-import React from "react";
-import YourBotArmy from "./YourBotArmy";
+import React, { useState, useEffect } from "react";
 import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+import BotSpecs from "../components/BotSpecs";
 
 function BotsPage() {
-  //start here with your code for step one
+    const [botCollection, setBotCollection] = useState([]);
+    const [filteredCollection, setFilteredCollection] = useState([]);
+    const [botArmy, setBotArmy] = useState([]);
+    const [collectionVisible, setCollectionVisible] = useState(true);
+    const [botSpecs, setBotSpecs] = useState({});
 
-  return (
-    <div>
-      <YourBotArmy />
-      <BotCollection />
-    </div>
-  )
-}
+    useEffect(() => {
+        fetch("http://localhost:3000/bots")
+            .then((res) => res.json())
+            .then((bots) => {
+                setBotCollection(bots);
+                setFilteredCollection(bots);
+            });
+    }, []);
+
+    function addToArmy(bot) {
+        const newCollection = filteredCollection.filter(
+            (card) => card.bot_class !== bot.bot_class
+        );
+        setFilteredCollection(newCollection);
+        setBotArmy([...botArmy, bot]);
+        setCollectionVisible(true);
+    };
+
+    function removeFromArmy(bot) {
+        const newArmy = botArmy.filter((card) => card.id !== bot.id);
+        const armyClasses = newArmy.map((bot) => bot.bot_class);
+        const newCollection = botCollection.filter(
+            (bot) => !armyClasses.includes(bot.bot_class)
+        );
+        setBotArmy(newArmy);
+        setFilteredCollection(newCollection);
+    };
+
+    function deleteBot(bot) {
+        const newCollection = botCollection.filter((card) => card !== bot);
+        const newFilteredCollection = filteredCollection.filter(
+            (card) => card !== bot
+        );
+        const newArmy = botArmy.filter((card) => card !== bot);
+        setBotCollection(newCollection);
+        setFilteredCollection(newFilteredCollection);
+        setBotArmy(newArmy);
+
+        fetch(`http://localhost:3000/bots/${bot.id}`, {
+            method: "DELETE",
+        })
+            .then((res) => res.json())
+            .then((data) => alert("bot deleted successfully!"));
+    };
+
+    function displayBotSpecs(bot) {
+        setCollectionVisible(false);
+        setBotSpecs(bot);
+    };
+
+    function botsCollection() {
+        setCollectionVisible(true);
+    };
+
+    return (
+        <div>
+            <YourBotArmy bots={botArmy} action={removeFromArmy} removeCard={deleteBot} />
+            {collectionVisible ? (
+                <BotCollection
+                    botCollection={filteredCollection}
+                    action={displayBotSpecs}
+                    removeCard={deleteBot} />
+            ) : (
+                <BotSpecs bot={botSpecs} back={botsCollection} enlist={addToArmy} />
+            )}
+        </div>
+    );
+};
 
 export default BotsPage;
-
